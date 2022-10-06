c13162c64b0c3c608a588ee6972b15789bc5f9fb
Matthias Achtelik <matthias.achtelik@mail.schwarz>
2022-10-06 14:18:30 +0200

    - Init

---
+++ src/main/java/schwarz/it/achtelik/gameoflife/GameOfLifeApplication.java
@@ -0,0 +1,13 @@
+package schwarz.it.achtelik.gameoflife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameOfLifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/schwarz/it/achtelik/gameoflife/GameOfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package schwarz.it.achtelik.gameoflife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
